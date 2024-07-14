6f024a6a6eef4d4bc6fdbdf9ee6c68684bdb113e
rehan1124 <rehan.11.24@gmail.com>
2024-07-14 22:51:45 +0530

    New test

---
+++ tests/login.spec.ts
@@ -0,0 +1,28 @@
+// test.user1@gmail.com  test.user1
+import { test, expect, Browser, Page, Locator } from "@playwright/test";
+import { webkit, chromium } from "@playwright/test";
+
+test("Login test", async () => {
+  const browser: Browser = await chromium.launch({ headless: false });
+  const page: Page = await browser.newPage();
+  await page.goto(
+    "https://naveenautomationlabs.com/opencart/index.php?route=account/login"
+  );
+
+  const email: Locator = await page.locator("#input-email");
+  const password: Locator = await page.locator("#input-password");
+  const loginBtn: Locator = await page.locator("[value='Login']");
+
+  await email.fill("test.user1@gmail.com");
+  await password.fill("test.user1");
+  await loginBtn.click();
+
+  const pageTitle = await page.title();
+  console.log(`Page title: ${pageTitle}`);
+
+  await page.screenshot({ path: "Screenshots/Homepage.png" });
+
+  expect(pageTitle).toEqual("My Account");
+
+  await browser.close();
+});
