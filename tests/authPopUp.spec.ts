aadf1c4e5b77123928dd1d5b1c593a3965d2cca2
rehan1124 <rehan.11.24@gmail.com>
2024-07-17 22:38:40 +0530

    Soft assertions and test-info

--- tests/authPopUp.spec.ts
+++ tests/authPopUp.spec.ts
@@ -12,7 +12,7 @@ function createAuthToken(username: String, password: String) {
   return "Basic " + btoa(`${username}:${password}`);
 }
 
-test("Authentication pop-up", async () => {
+test("Authentication pop-up", async ({}, testInfo) => {
   const browser: Browser = await webkit.launch({ headless: false });
   const context: BrowserContext = await browser.newContext();
   const page: Page = await context.newPage();
@@ -29,9 +29,13 @@ test("Authentication pop-up", async () => {
   const loginSuccessMsgText: String = await loginSuccessMsg.innerText();
   console.log(`Success msg: ${loginSuccessMsgText}`);
 
-  expect(loginSuccessMsgText).toEqual(
-    "Congratulations! You must have the proper credentials."
-  );
+  expect
+    .soft(loginSuccessMsgText)
+    .toEqual("Congratulations! You must have the proper credentials.");
+
+  console.log("Title:", testInfo.title);
+  console.log("TestId:", testInfo.testId);
+  console.log("Title path:", testInfo.titlePath);
 
   await context.close();
   await browser.close();
